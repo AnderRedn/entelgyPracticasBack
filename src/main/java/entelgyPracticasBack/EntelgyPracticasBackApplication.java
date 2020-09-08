40f6fcfa697537e2e19d94ec88831e32c36c1508
Ander <ander1olivas@gmail.com>
2020-09-08 08:34:32 +0200

    First commit

---
+++ src/main/java/entelgyPracticasBack/EntelgyPracticasBackApplication.java
@@ -0,0 +1,13 @@
+package entelgyPracticasBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntelgyPracticasBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntelgyPracticasBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/entelgyPracticasBack/EntelgyPracticasBackApplicationTests.java
@@ -0,0 +1,13 @@
+package entelgyPracticasBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntelgyPracticasBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
